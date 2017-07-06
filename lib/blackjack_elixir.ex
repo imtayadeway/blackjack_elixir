e61661ff92f22b9c84c6fe08303bc71b3828dec6
Tim Wade <hello@timjwade.com>
2017-07-05 19:24:29 -0700

    New elixir project

---
+++ lib/blackjack_elixir.ex
@@ -0,0 +1,18 @@
+defmodule BlackjackElixir do
+  @moduledoc """
+  Documentation for BlackjackElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BlackjackElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/blackjack_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule BlackjackElixirTest do
+  use ExUnit.Case
+  doctest BlackjackElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
