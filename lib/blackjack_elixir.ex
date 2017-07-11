a258111bda8b946298e98ed2e77e39ec76498e89
Tim Wade <hello@timjwade.com>
2017-07-10 20:21:25 -0700

    wip

--- lib/blackjack_elixir.ex
+++ lib/blackjack_elixir.ex
@@ -1,5 +1,7 @@
 defmodule BlackjackElixir do
   def main(_args) do
     IO.puts("hello")
+    IO.gets("hit or stay? [h, s]:")
+    IO.puts("")
   end
 end
--- test/blackjack_elixir_test.exs
+++ test/blackjack_elixir_test.exs
@@ -2,8 +2,25 @@ defmodule BlackjackElixirTest do
   use ExUnit.Case
   doctest BlackjackElixir
 
-  test "make builds the application" do
-    assert {_, 0} = System.cmd("make", ["blackjack"])
-    assert {"hello\n", 0} = System.cmd(Path.join(File.cwd!, "blackjack_elixir"), [])
+  import ExUnit.CaptureIO
+
+ # test "make builds the application" do
+ #   assert {_, 0} = System.cmd("make", ["blackjack"])
+ #   assert {"hello\n", 0} = System.cmd(Path.join(File.cwd!, "blackjack_elixir"), [])
+ # end
+
+  test "I can hit or stay" do
+    pid = spawn(fn ->
+        System.cmd(Path.join(File.cwd!, "blackjack_elixir"), [],
+        into: IO.stream(:stdio, :line)) end)
+
+    Process.sleep(10000)
+
+    IO.inspect(Process.alive?(pid), label: "alive?")
+    IO.puts(:stdio, "h")
+    Process.sleep(1000)
+    IO.inspect(Process.alive?(pid), label: "alive?")
+
+    Process.sleep(10000)
   end
 end
